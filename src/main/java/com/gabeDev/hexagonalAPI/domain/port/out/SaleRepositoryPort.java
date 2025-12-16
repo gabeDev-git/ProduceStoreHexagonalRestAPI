cb0aa5e01f6e138c46c31b35f85fbb00a40f728d
gabeDev <gabrielamorym2@gmail.com>
2025-12-16 02:29:40 -0300

    fix: SaleRepositoryPort returning wrong types

--- src/main/java/com/gabeDev/hexagonalAPI/domain/port/out/SaleRepositoryPort.java
+++ src/main/java/com/gabeDev/hexagonalAPI/domain/port/out/SaleRepositoryPort.java
@@ -2,15 +2,18 @@ package com.gabeDev.hexagonalAPI.domain.port.out;
 
 import com.gabeDev.hexagonalAPI.domain.model.Sale;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SaleRepositoryPort {
 
     Sale save(Sale sale);
     List<Sale> findAll();
-    Sale findById(UUID id);
-    Sale deleteById(UUID id);
+    List<Sale> findByTimeframe(Instant timeframe);
+    Optional<Sale> findById(UUID id);
+    void deleteById(UUID id);
 
 
 }
