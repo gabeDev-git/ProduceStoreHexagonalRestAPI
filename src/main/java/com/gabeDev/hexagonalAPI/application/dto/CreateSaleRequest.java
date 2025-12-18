faf86a52d12306691a5916b3892eb79cfaacae25
gabeDev <gabrielamorym2@gmail.com>
2025-12-18 01:13:39 -0300

    refactor: added minor validation to Sale POST dto

--- src/main/java/com/gabeDev/hexagonalAPI/application/dto/CreateSaleRequest.java
+++ src/main/java/com/gabeDev/hexagonalAPI/application/dto/CreateSaleRequest.java
@@ -1,9 +1,11 @@
 package com.gabeDev.hexagonalAPI.application.dto;
 
 import com.gabeDev.hexagonalAPI.domain.model.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.HashMap;
 import java.util.UUID;
 
-public record CreateSaleRequest(HashMap<UUID, Integer> checkoutMap, PaymentMethod paymentMethod) {
+public record CreateSaleRequest(@NotNull HashMap<UUID, Integer> checkoutMap,
+                                @NotNull PaymentMethod paymentMethod) {
 }
